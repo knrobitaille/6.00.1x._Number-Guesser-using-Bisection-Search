5a9484ecfb085d367ff714bb41906412ba0a2b8e
krobitaille93 <52327814+krobitaille93@users.noreply.github.com>
2019-09-04 12:52:31 -0400

    Update W2EGuessNumber.py

--- W2EGuessNumber.py
+++ W2EGuessNumber.py
@@ -1,20 +1,17 @@
-"""
-Created on Fri Jun 21 10:04:20 2019
-
-@author: KR
-"""
-
-
-print("Please think of a number between 0 and 100!")
+print("Please think of a secret number between 0 and 100!")
 low = 0
 high = 100
-ans = (high + low) / 2
+ans = int((high + low) / 2)
 response = ""
 guessed = False
 
 while not guessed:
-    print("Is your secret number " + str(int(ans)) + "?")
+    print("Is your secret number " + str(ans) + "?")
     response = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+    while response not in ['h','l','c']:
+        print("Sorry, I did not understand your input.")
+        print("Is your secret number " + str(int(ans)) + "?")  
+        response = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
     if response == 'h':
         high = ans
     elif response == 'l':
@@ -23,13 +20,6 @@ while not guessed:
         print("Game over. Your secret number was: " + str(int(ans)))
         guessed == True
         break
-    else:
-         print("Sorry, I did not understand your input.")
-         print("Is your secret number " + str(int(ans)) + "?")
-         response = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")     
-         if response == 'c':
-             print("Game over. Your secret number was: " + str(int(ans)))
-             guessed == True
-             break
     ans = int(float((high + low) / 2))
-  
+    
+    
